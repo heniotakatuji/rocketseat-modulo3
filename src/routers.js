1e121c505f2c40ff1733f3afcc49b29b52451482
heniotakatuji <henioricardo@hotmail.com>
2019-02-26 20:39:04 -0300

    segunda versao

--- src/routers.js
+++ src/routers.js
@@ -1,5 +1,14 @@
 const express = require('express')
 
+const authMiddleware = require('./app/middlewares/auth')
+
 const routes = express.Router()
+
+const controllers = require('./app/controllers')
+
+routes.post('/users', controllers.UserController.store)
+routes.post('/session', controllers.SessionController.store)
+
+routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseconfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
+
+    this.database()
     this.middlewares()
     this.routes()
+  }
+
+  database () {
+    mongoose.connect(databaseconfig.uri, {
+      useCreateIndex: true
+    })
   }
 
   middlewares () {
@@ -13,7 +23,7 @@ class App {
   }
 
   routes () {
-    this.express.use(require('./routes'))
+    this.express.use(require('./routers'))
   }
 }
 
